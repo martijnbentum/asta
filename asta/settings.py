9e347c11037a125dca35bdb944432028187ebcc6
martijnbentum <bentummartijn@gmail.com>
2022-05-18 10:28:18 +0200

    removed secret key not needed

--- asta/settings.py
+++ asta/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#l_il&uq_40=5hg*99_=rao39adtzb2#okk(zxzoppw70s_#@$'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'data.apps.DataConfig',
+	'metadata.apps.MetadataConfig',
 ]
 
 MIDDLEWARE = [
