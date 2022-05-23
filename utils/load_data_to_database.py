8d1db6196882b067adf289651b07d96b2cd66094
martijnbentum <bentummartijn@gmail.com>
2022-05-23 19:30:44 +0200

    added germany to countries

--- utils/load_data_to_database.py
+++ utils/load_data_to_database.py
@@ -1,6 +1,8 @@
 from . import read_tsv
+from . import read_kloeke
 # from django.core.exceptions import DoesNotExist
 from texts.models import Country
+
 
 def load_city():
 	pass
@@ -9,7 +11,7 @@ def load_area():
 	pass
 
 def load_country():
-	for name in 'Netherlands,Belgium'.split(','):
+	for name in 'Netherlands,Belgium,Germany'.split(','):
 		try: Country.objects.get(name = name)
 		except Country.DoesNotExist:
 			print('creating country',name)
