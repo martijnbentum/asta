75f5603c33f7f78c5b4d34b7db84ce526afaad37
martijnbentum <bentummartijn@gmail.com>
2022-05-23 09:04:12 +0200

    added location and confidence information to the transcription

--- utils/read_xml.py
+++ utils/read_xml.py
@@ -5,7 +5,7 @@ def tag_to_element(tag,xml):
 	'''extract a specific element for xml object'''
 	temp = list(xml.iter('{*}' + tag))
 	if len(temp) == 0: return None
-	if len(temp) > 1: print('found more than one elements',tag,len(temp))
+	# if len(temp) > 1: print('found more than one elements',tag,len(temp))
 	return temp[0]
 	
 def points_to_avg_y(points):
@@ -18,23 +18,35 @@ def points_to_avg_y(points):
 def points_to_left_x(points):
 	return int(points.split(' ')[0].split(',')[0])
 
+def points_to_right_x(points):
+	return int(points.split(' ')[1].split(',')[0])
+
 def _get_transcription_from_textline(textline):
+	i= textline.get('id')
 	textequiv = tag_to_element('TextEquiv',textline)
 	uc = tag_to_element('Unicode',textequiv)
 	transcription = uc.text
+	if not transcription: return False
 	conf = textequiv.get('conf')
 	baseline= tag_to_element('Baseline',textline)
 	points = baseline.get('points')
-	y = points_to_y(points)
-	x = points_to_left_x(points)
+	avg_y = points_to_avg_y(points)
+	left_x = points_to_left_x(points)
+	right_x = points_to_right_x(points)
 	try: conf = round(float(conf),4)
 	except:pass
-	return transcription, conf, x, y
+	d = {'ocr_line_id':i,'transcription':transcription, 'confidence':conf}
+	d.update({'left_x':left_x, 'right_x':right_x, 'avg_y':avg_y})
+	return d
 
 def extract_transcription_from_xml(filename):
 	xml = etree.fromstring(open(filename).read())
 	transcription = []
 	for x in xml.iter('{*}TextLine'):
-			line = _get_transcription_from_textline(x)
-			transcription.append(line)
+		line = _get_transcription_from_textline(x)
+		if not line: continue
+		transcription.append(line)
+	transcription = sorted(transcription, key = lambda x:x['avg_y'])
 	return transcription 
+
+
