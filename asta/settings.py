9752fee1d0bbed179c5faf8037ebed3b19022114
martijnbentum <bentummartijn@gmail.com>
2022-05-23 13:35:30 +0200

    changed appname metadata to texts

--- asta/settings.py
+++ asta/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'metadata.apps.MetadataConfig',
+	'texts.apps.TextsConfig',
 ]
 
 MIDDLEWARE = [
