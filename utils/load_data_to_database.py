01e292df3fe10eada3e5b2147233449fb663fa32
martijnbentum <bentummartijn@gmail.com>
2022-05-23 13:37:19 +0200

    code to load data sqlite database

---
+++ utils/load_data_to_database.py
@@ -0,0 +1,21 @@
+from . import read_tsv
+# from django.core.exceptions import DoesNotExist
+from texts.models import Country
+
+def load_city():
+	pass
+
+def load_area():
+	pass
+
+def load_country():
+	for name in 'Netherlands,Belgium'.split(','):
+		try: Country.objects.get(name = name)
+		except Country.DoesNotExist:
+			print('creating country',name)
+			c = Country(name=name)
+			c.save()
+		else:print(name,'already exists')
+	
+
+
