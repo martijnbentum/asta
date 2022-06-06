bafbb0d3f9cb0b0e95ec56207a7f955ca3699ad3
martijnbentum <bentummartijn@gmail.com>
2022-06-06 12:46:53 +0200

    added code to transcribe recordings and save results; sequential decoding is suboptimal with pipeline (should be done with dataset?) - output stored in file and databse

--- utils/wav2vec2.py
+++ utils/wav2vec2.py
@@ -1,5 +1,10 @@
 from transformers import Wav2Vec2ForCTC
 from transformers import Wav2Vec2Processor
+from texts.models import Recording, Recordingtype, Asr, Transcription
+from texts.models import Transcriptiontype
+import os 
+
+fremy_model1 = 'FremyCompany/xls-r-2b-nl-v2_lm-5gram-os'
 
 ''' 
 More info about pipelines for ASR see:
@@ -33,3 +38,94 @@ def load_pipeline(recognizer_dir=None, chunk_length_s = 10, device = -1):
 		device = device
 	)
 	return pipeline
+
+def decode_recording(recording, pipeline, start=0.0, end=None):
+	audio = recording.load_audio(start,end)
+	output = pipeline(audio, return_timestamps = 'word')
+	return output 
+	
+
+def load_or_make_asr(pipeline,model_name='', comments = ''):
+	directory= pipeline.tokenizer.name_or_path
+	try: asr = Asr.objects.get(directory = directory)
+	except Asr.DoesNotExist:
+		print('creating asr',directory)
+		asr = Asr(
+			directory=directory,
+			modelname = model_name,
+			comments = comments,
+			tokenizer_description = str(pipeline.tokenizer),
+			model_description = str(pipeline.model),
+			feature_extractor_description = str(pipeline.feature_extractor)
+		)
+		asr.save()
+	else:
+		m = 'loading asr object corresponding to a pipeline loaded from:\n'
+		print(m,directory)
+	return asr
+
+
+def make_decoding_output_filename(asr,recording):
+	filename = '../WAV2VEC2_TRANSCRIPTIONS/'
+	filename += 'record_id-' + str(recording.record_id)
+	filename += '_recording-' + str(recording.pk)
+	filename += '_asr-' + str(asr.pk)
+	return filename
+
+def save_pipeline_output_to_file(output, asr, recording):
+	filename = make_decoding_output_filename(asr,recording)
+	table = pipeline_output2table(output)
+	print('saving to:',filename)
+	with open(filename,'w') as fout:
+		fout.write(table2str(table))
+
+def save_pipeline_output_to_transcription(output, asr, recording):
+	tt = Transcriptiontype.objects.get(name = 'asr')
+	table = pipeline_output2table(output)
+	table_str = table2str(table)
+	start = table[0][1]
+	end  = table[-1][2]
+	transcription = Transcription(
+		recording = recording,
+		text = output['text'],
+		transcription_type = tt,
+		asr = asr,
+		asr_words = table_str,
+		start_time = start,
+		end_time = end,
+	)
+	transcription.save()
+	return transcription
+
+def pipeline_output2table(output):
+	table = []
+	for d in output['chunks']:
+		start, end = d['timestamp']
+		table.append([d['text'], start, end])
+	return table
+
+def table2str(table):
+	output = []
+	for line in table:
+		output.append('\t'.join(list(map(str,line))))
+	return '\n'.join(output)
+
+def transcribe_recordings(recordings,pipeline = None, overwrite = False):
+	if not pipeline:
+		print('loading default pipeline, uses GPU')
+		pipeline = load_pipeline(device = 0)
+	asr = load_or_make_asr(pipeline)
+	nrecordings = len(recordings)
+	for i,recording in enumerate(recordings):
+		filename = make_decoding_output_filename(asr,recording)
+		if not overwrite and os.path.isfile(filename):
+			print('SKIPPING:',i,recording,'filename exists:',filename)
+			continue
+		else: print('decoding:',i,recording,nrecordings)
+		output = decode_recording(recording, pipeline)
+		save_pipeline_output_to_file(output, asr, recording)
+		save_pipeline_output_to_transcription(output, asr, recording)
+		
+
+		
+
