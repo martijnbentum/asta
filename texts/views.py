bf1bcc354f73fc7c212070199ac333962703a004
martijnbentum <bentummartijn@gmail.com>
2022-07-21 07:37:45 +0200

    changed for serving site

--- texts/views.py
+++ texts/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from . models import Recording
+from utils import align_ocr_asr as aoa
 
 # Create your views here.
+
+def hello_world(request):
+    return render(request, 'texts/hello_world.html')
+
+def play(request,pk = 2):
+    recording = Recording.objects.get(pk = pk)
+    a = aoa.Align(recording)
+    o = a.ocr_lines[0]
+    ocr = recording.ocrs[0]
+    args = {'a':a,'o':o,'ocr':ocr}
+    return render(request, 'texts/play.html',args)
+
+
+    
+    
