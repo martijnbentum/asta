026498904415a3e232c3382b3ffda2a9a75e0601
martijnbentum <bentummartijn@gmail.com>
2022-07-21 17:56:42 +0200

    sketch for audio ocr annotation

--- texts/views.py
+++ texts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from . models import Recording
 from utils import align_ocr_asr as aoa
+import random
 
 # Create your views here.
 
@@ -8,11 +9,20 @@ def hello_world(request):
     return render(request, 'texts/hello_world.html')
 
 def play(request,pk = 2):
-    recording = Recording.objects.get(pk = pk)
+    post = request.method 
+    if request.method == 'POST':
+        if 'quality' in request.POST.keys():print('quality',request.POST['quality'])
+        else:print(request.POST.keys(),'quality not found')
+    if pk:
+        recording = Recording.objects.get(pk = pk)
+    else: 
+        recordings = Recording.objects.filter(ocr_transcription_available=True)
+        recording = random.sample(list(recordings),1)[0]
     a = aoa.Align(recording)
-    o = a.ocr_lines[0]
+    if not hasattr(a,'ocr_lines'): o = ''
+    else: o = random.sample(a.filter_ocr_lines(),1)[0]
     ocr = recording.ocrs[0]
-    args = {'a':a,'o':o,'ocr':ocr}
+    args = {'a':a,'o':o,'ocr':ocr,'post':post}
     return render(request, 'texts/play.html',args)
 
 
