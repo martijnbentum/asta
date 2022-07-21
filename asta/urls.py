8c987be30b3936842b584b31e3eb247fc914fc99
martijnbentum <bentummartijn@gmail.com>
2022-07-21 07:37:28 +0200

    changed for serving site

--- asta/urls.py
+++ asta/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path, include
+from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+	path('', include('texts.urls')),
 ]
+
+x = re_path(r'vol/(?P<filename>.*)$', views.protected_media,
+    name='protected_media')
+urlpatterns.append(x)
