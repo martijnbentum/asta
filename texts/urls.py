5d19e5a729fcfe0a03ed47660f9ccc6988772794
martijnbentum <bentummartijn@gmail.com>
2022-07-27 15:54:37 +0200

    added app name and play url

--- texts/urls.py
+++ texts/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'texts'
+
 urlpatterns = [
 	# path('',views.hello_world,name='hello_world'),
 	path('',views.play,name='play'),
+	path('play/',views.play,name='play'),
 ]
