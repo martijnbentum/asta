137b8c41140e69f90cd4d202ba359f994daa1309
Martijn Bentum <bentummartijn@gmail.com>
2022-08-04 16:15:23 +0200

    added code to handle different scenarios for annotating

--- utils/select.py
+++ utils/select.py
@@ -1,4 +1,4 @@
-from texts.models import Recording
+from texts.models import Recording, AnnotationUserInfo
 from collections import Counter
 from utils import align
 import random
@@ -127,18 +127,62 @@ def get_dutch_areas():
     recordings = get_recordings_from_country(country='Netherlands')
     return dict(Counter([x.area for x in recordings if x.area]))
     
-    
-'''
-def args_to_ocrline(request, location= '', location_type= '', exclude = 'None',
-    minimum_match = 35, perc_lines = 20, record_index = 0, line_index = 0):
-'''
-def args_to_ocrline(args):
-    start = time.time()
-    print(args, delta(start))
+
+def get_recordings_with_location_info(args):
     if args['location_type'] == 'area':
         recordings = get_recordings_with_areas_list([args['location']])
     if args['location_type'] == 'province':
         recordings = get_recordings_with_province_list([args['location']])
+    return recordings
+
+def get_all_finished_recording_pks_from_all_users():
+    output = []
+    for aui in AnnotationUserInfo.objects.all():
+        if aui.finished_pks: 
+            pks = aui.finished_pks.split(',')
+            pks = [pk for pk in pks if pk not in output]
+            output.extend(pks)
+    
+def get_recordings_with_annotation_user_info(args):
+    aui = args['annotation_user_info']
+    if args['resume'] == 'true' and aui.current_recording: 
+        args['location'] = aui.current_location
+        args['location_type'] = aui.current_location_type
+    recordings = get_recordings_with_location_info(args)
+    if args['resume'] == 'true' and aui.current_recording:
+        print('resuming from last point',args['resume'])
+        args['resume'] = 'false'
+        print(args['resume'])
+        pks = [x.pk for x in recordings]
+        try: args['record_index'] = pks.index(aui.current_recording.pk)
+        except ValueError: pass
+        else: args['line_index'] = aui.current_line_index 
+    return recordings, args
+
+def filter_recordings_with_exclude(exclude, recordings):    
+    if exclude == 'none': return recordings
+    if exclude == 'annotated by me' and aui.finished_recording_pks:
+        finished_pks = aui.finished_recording_pks.split(',')
+    elif exclude == 'annotated by anyone':
+        finished_pks = get_all_finished_recording_pks_from_all_users()
+    print(exclude)
+    temp = [x for x in recordings if x.pk not in finished_recording_pks]
+    if temp: recordings = temp
+    return recordings
+
+def _update_annotation_user_info(args,recording):
+    aui = args['annotation_user_info']
+    aui.current_line_index = args['line_index']
+    aui.current_recording= recording
+    aui.current_location = args['location']
+    aui.current_location_type = args['location_type']
+    aui.save()
+
+def args_to_ocrline(args):
+    start = time.time()
+    print(args, delta(start))
+    recordings, args = get_recordings_with_annotation_user_info(args)
+    recordings = filter_recordings_with_exclude(args['exclude'], recordings)
     recording = recordings[args['record_index']]
     mismatch = 100 - args['minimum_match']
     print('mismatch',mismatch,delta(start))
@@ -147,11 +191,15 @@ def args_to_ocrline(args):
     print('n ocr lines',len(ocr_lines),delta(start))
     if args['line_index'] >= len(ocr_lines) or len(ocr_lines) == 0: 
         print(recording)
+        args['annotation_user_info'].add_finished_recording_pk(recording)
         args['record_index'] +=1
         if args['record_index'] >= recordings.count(): 
             return 'done with all recordings'
         args['line_index'] = 0
         return args_to_ocrline(args)
+    _update_annotation_user_info(args,recording)
+    print('ocr_lines indices',[x.ocrline_index for x in ocr_lines])
+    print('line_index',args['line_index'])
     args['ocrline'] = ocr_lines[args['line_index']]
     args['nocrlines'] = len(ocr_lines)
     args['line_index'] +=1
