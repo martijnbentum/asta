d290c9406f2ca4d3b8cbfba8f9104675482ca276
Martijn Bentum <bentummartijn@gmail.com>
2022-08-08 10:53:21 +0200

    removed play view

--- texts/urls.py
+++ texts/urls.py
@@ -10,7 +10,6 @@ annotate_url += '<str:exclude_transcriptions>/'
 urlpatterns = [
 	# path('',views.hello_world,name='hello_world'),
 	path('',views.home,name='home'),
-	path('play/',views.play,name='play'),
 	path('select_province/',views.select_province,name='select_province'),
 	path('select_area/',views.select_area,name='select_area'),
 	path('annotate/',views.annotate,name='annotate'),
--- texts/views.py
+++ texts/views.py
@@ -3,23 +3,6 @@ from . models import Recording, Transcription, Ocr, Asr, Annotation, AnnotationU
 from utils import align, select
 import random
 
-def play(request,pk = 2):
-    post = request.method 
-    if request.method == 'POST':
-        if 'quality' in request.POST.keys():
-            print('quality',request.POST['quality'])
-        else:print(request.POST.keys(),'quality not found')
-    if pk:
-        recording = Recording.objects.get(pk = pk)
-    else: 
-        recordings = Recording.objects.filter(ocr_transcription_available=True)
-        recording = random.sample(list(recordings),1)[0]
-    a = align.Align(recording)
-    if not hasattr(a,'ocr_lines'): o = ''
-    else: o = random.sample(a.filter_ocr_lines(),1)[0]
-    ocr = recording.ocrs[0]
-    args = {'a':a,'o':o,'ocr':ocr,'post':post}
-    return render(request, 'texts/play.html',args)
 
 def home(request):
     args = {}
