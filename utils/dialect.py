5ae16b11b63959677f3f06a2881f89290f0a33c6
martijnbentum <bentummartijn@gmail.com>
2022-08-31 12:49:58 +0200

    module to store dialect information

---
+++ utils/dialect.py
@@ -0,0 +1,4 @@
+dialect_groups = {'Nedersaksisch':['Acht','Ov-O']}
+dialect_groups['Limburgs'] = ['NLimb-Z','NLimb-M','NLimb-N']
+dialect_groups['Noord-Brabants'] = ['NBra-M','NBra-Z','NBra-W','NBra-O']
+dialect_groups['Zuid-Hollands'] = ['ZHol-O','ZHol-W','ZHol-N']
